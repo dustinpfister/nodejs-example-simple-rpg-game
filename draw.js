21c4c47104606d389f78af6eb436315a6976bf83
dustinpfister <dustin.pfister@gmail.com>
2020-03-13 13:01:47 -0400

    cleanup

---
+++ draw.js
@@ -0,0 +1,39 @@
+
+let setCur = (x, y, out) => {
+    x = x || 0;
+    y = y || 0;
+    out.write('\u001b[' + y + ';' + x + 'H');
+}
+
+let clearScreen = (out) => {
+    out.write('\u001b[2J');
+}
+let colorsSet = (out) => {
+    out.write('\u001b[47m');
+    out.write('\u001b[30m');
+};
+let colorsDefault = (out) => {
+    out.write('\u001b[39m\u001b[49m');
+};
+
+// draw the area and at symbol
+module.exports = (opt, out) => {
+    out = out || process.stdout;
+    opt = opt || {};
+    opt.x = opt.x || 1;
+    opt.y = opt.y || 1;
+    clearScreen(out);
+    setCur(1, 1, out);
+    colorsSet(out);
+    // draw area
+    out.write('..........\n');
+    out.write('..........\n');
+    out.write('..........\n');
+    out.write('..........\n');
+    out.write('move: wasd; exit: x');
+    // draw at symbol
+    setCur(opt.x, opt.y, out);
+    out.write('@');
+    colorsDefault(out);
+    setCur(0, 6, out);
+};
--- game.js
+++ game.js
@@ -1,39 +1,6 @@
-let out = process.stdout;
-// Set the position of a cursor
-let setCur = (x, y) => {
-    x = x || 0;
-    y = y || 0;
-    out.write('\u001b[' + y + ';' + x + 'H');
-}
-let clearScreen = () => {
-    out.write('\u001b[2J');
-}
-let colorsSet = () => {
-    out.write('\u001b[47m');
-    out.write('\u001b[30m');
-};
-let colorsDefault = () => {
-    out.write('\u001b[39m\u001b[49m');
-};
-// draw the area and at symbol
-let draw = (opt) => {
-    opt = opt || {};
-    opt.x = opt.x || 1;
-    opt.y = opt.y || 1;
-    clearScreen();
-    setCur(1, 1);
-    colorsSet();
-    // draw area
-    out.write('..........\n');
-    out.write('..........\n');
-    out.write('..........\n');
-    out.write('..........\n');
-    out.write('move: wasd; exit: x');
-    // draw at symbol
-    setCur(opt.x, opt.y);
-    out.write('@');
-    colorsDefault();
-};
+let out = process.stdout,
+draw = require('./draw.js');
+
 // start position
 let pos = {
     x: 1,
@@ -42,9 +9,8 @@ let pos = {
     h: 4
 };
 draw(pos);
-setCur(1, 6);
 // set in raw mode and capture key strokes
-process.stdin.setRawMode(true)
+process.stdin.setRawMode(true);
 process.stdin.on('data', (data) => {
     let input = data.toString().trim();
     if (input === 'd') {
@@ -64,6 +30,5 @@ process.stdin.on('data', (data) => {
     }
     pos.x = pos.x > pos.w ? pos.w: pos.x;
     pos.y = pos.y > pos.h ? pos.h: pos.y;
-    draw(pos);
-    setCur(1, 6);
+    draw(pos, out);
 });
