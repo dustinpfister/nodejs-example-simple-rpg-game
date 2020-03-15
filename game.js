d63a48c6aa418095963b1931da293ce7a623ad18
dustinpfister <dustin.pfister@gmail.com>
2020-03-15 11:58:44 -0400

    input.js

--- game.js
+++ game.js
@@ -2,52 +2,29 @@ let u = require('./lib/utils.js'),
 draw = require('./lib/draw.js'),
 stateMod = require('./lib/state.js'),
 playerMod = require('./lib/player.js'),
-enemies = require('./lib/enemies.js');
+enemies = require('./lib/enemies.js'),
+inputHandler = require('./lib/input.js');
 
 stateMod.loadState()
 .then((state) => {
-
-    let movementHandler = (state, input) => {
-        let player = state.player,
-        tempX = player.x,
-        tempY = player.y;
-        if (input === 'd') {
-            tempX += 1;
-        }
-        if (input === 'a') {
-            tempX -= 1;
-        }
-        if (input === 'w') {
-            tempY -= 1;
-        }
-        if (input === 's') {
-            tempY += 1;
-        }
-        if (input === 'x') {
-            process.exit();
-        }
-        // update player
-        playerMod.update(state, tempX, tempY);
-        // spawn and update enemies
-        enemies.spawnEnemy(state);
-        enemies.updateEnemies(state);
-        // if player dies, start over
-        if (player.hp === 0) {
-            let newState = stateMod.newState();
-            state = Object.assign(state, newState);
-            draw.newScreen(state);
-        }
-        // save state
-        stateMod.saveState(state);
-    };
-
     //set in raw mode and capture key strokes
     process.stdin.setRawMode(true);
     process.stdin.on('data', (data) => {
         let input = data.toString().trim();
-        movementHandler(state, input);
-        draw.updateScreen(state);
+        inputHandler(state, input, {
+            onPlayerDead: (state) => {
+                let player = state.player;
+                if (player.hp === 0) {
+                    let newState = stateMod.newState();
+                    state = Object.assign(state, newState);
+                    draw.newScreen(state);
+                }
+            },
+            onTurnOver: (state) => {
+                draw.updateScreen(state);
+                stateMod.saveState(state);
+            }
+        });
     });
     draw.newScreen(state);
-
 });
